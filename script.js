448fa5e88bc0247369332dfe0b84c51df45e7ca4
Joel Amaefula <joelamaefula@gmail.com>
2022-11-17 10:15:37 +0100

    make the script for adding new todo list items

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ $(document).ready(function () {
     const checkboxLabel = $(".label-control span");
     const checkboxPara = $(".item-wrap p");
 
+    const listItemContainer = $("#list-content");
+    const additemBtn = $("#add-item-btn");
+    const additemInput = $("#add-item-input");
+
 
     checkboxLabel.click(function() {
         $(this).parent().next().toggleClass("checked");
@@ -11,6 +15,34 @@ $(document).ready(function () {
         $(this).prev().children("span").click();
     })
 
+    /* function for addding new list item */
+    additemBtn.click(function() {
+        let inputText = additemInput.val();
+
+        if (inputText === "") {
+            alert("no text inputted");
+        } else {
+            let itemWrap = $("<div></div>").addClass("item-wrap");
+            let checkboxLabel = $("<label></label>").addClass("checkbox-wrapper label-control");
+            let checkbox = $("<input></input>").attr("type", "checkbox");
+            let checkboxSpan = $("<span></span>").addClass("checkmark");
+            let itemPara = $("<p></p>").text(inputText);
+
+            checkboxLabel.append(checkbox, checkboxSpan);
+
+            itemWrap.append(checkboxLabel, itemPara);
+
+            listItemContainer.append(itemWrap);
+
+            additemInput.val("");
+        }
+    })
+
+    additemInput.on("keydown", function(e){
+        if(e.which === 13){
+            additemBtn.click();
+        }
+    });
 
     
 });
