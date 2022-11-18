30f922b7217d7665063799359856105805594647
Joel Amaefula <joelamaefula@gmail.com>
2022-11-18 10:04:39 +0100

    create js theme change on icon click

--- script.js
+++ script.js
@@ -1,23 +1,51 @@
-$(document).ready(function () {
-    const checkboxLabel = $(".label-control span");
-    const checkboxPara = $(".item-wrap p");
+const checkboxLabel = $(".label-control span");
+const checkboxPara = $(".item-wrap p");
 
-    const listItemContainer = $("#list-content");
-    const additemBtn = $("#add-item-btn");
-    const additemInput = $("#add-item-input");
+const listItemContainer = $("#list-content");
+const additemBtn = $("#add-item-btn");
+const additemInput = $("#add-item-input");
+
+const body = $("body");
+
+const themeBtn = $("#theme-btn");
+
+$(document).ready(function () {
+
+    themeBtn.click(function () {
+
+        if (body.attr("web-theme") === "light") {
+            body.attr("web-theme", "dark");
+            $("#theme-btn img").attr("src", "images/icon-moon.svg");
+        } else {
+            body.attr("web-theme", "light");
+            $("#theme-btn img").attr("src", "images/icon-sun.svg");
+        }
+
+        bgChange();
+
+        setTheme(body.attr("web-theme"));
+
+        console.log("yes");
+    });
 
     /* function for addding new list item */
-    additemBtn.click(function() {
+    additemBtn.click(function () {
         let inputText = additemInput.val();
 
         if (inputText === "") {
             alert("no text inputted");
         } else {
             let itemWrap = $("<div></div>").addClass("item-wrap");
-            let checkboxLabel = $("<label></label>").addClass("checkbox-wrapper label-control");
+            let checkboxLabel = $("<label></label>").addClass(
+                "checkbox-wrapper label-control"
+            );
             let checkbox = $("<input></input>").attr("type", "checkbox");
-            let checkboxSpan = $("<span></span>").addClass("checkmark").attr("onclick", "checkToggle(this)");
-            let itemPara = $("<p></p>").text(inputText).attr("onclick", "checkPara(this)");
+            let checkboxSpan = $("<span></span>")
+                .addClass("checkmark")
+                .attr("onclick", "checkToggle(this)");
+            let itemPara = $("<p></p>")
+                .text(inputText)
+                .attr("onclick", "checkPara(this)");
 
             checkboxLabel.append(checkbox, checkboxSpan);
 
@@ -27,21 +55,63 @@ $(document).ready(function () {
 
             additemInput.val("");
         }
-    })
+    });
 
-    additemInput.on("keydown", function(e){
-        if(e.which === 13){
+    /* submits the  */
+    additemInput.on("keydown", function (e) {
+        if (e.which === 13) {
             additemBtn.click();
         }
     });
-
-    
 });
 
 function checkToggle(ele) {
     ele.parentElement.nextElementSibling.classList.toggle("checked");
+
+    /* changes the tod status attribute */
+    var todoStatus = ele.parentElement.parentElement.attributes["todo-status"];
+
+    if (todoStatus.value === "active") {
+        todoStatus.value = "completed";
+    } else {
+        todoStatus.value = "active";
+    }
 }
 
-function checkPara(ele){
+function checkPara(ele) {
     ele.previousElementSibling.children[1].click();
+}
+
+function bgChange() {
+    let backgroundImage = $(".bg-image");
+
+    if (body.attr("web-theme") === "light") {
+        if (window.outerWidth >= 1050) {
+            backgroundImage.attr("src", "images/bg-desktop-light.jpg");
+        } else {
+            backgroundImage.attr("src", "images/bg-mobile-light.jpg");
+        }
+    } else {
+        if (window.outerWidth >= 1050) {
+            backgroundImage.attr("src", "images/bg-desktop-dark.jpg");
+        } else {
+            backgroundImage.attr("src", "images/bg-mobile-dark.jpg");
+        }
+    }
+}
+
+/* stores a the new theme on theme button click */
+function setTheme(theme){
+    localStorage.setItem("userTheme", theme);
+}
+
+/* sets the stored theme on load */
+function getTheme() { 
+    if ( !(localStorage.getItem("userTheme") === null)) {
+        body.attr("web-theme", localStorage.getItem("userTheme"));
+    } else{
+        localStorage.setItem("userTheme", body.attr("web-theme"));
+    }
+
+    themeBtn.click();
 }
